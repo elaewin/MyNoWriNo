132b28056db9ebf7f366d41ec89db3f7d4a081c4
Corey Malek <coreymalek@coreys-macbook-pro.local>
2016-11-07 14:41:36 -0800

    Added buttons between view controllers

--- MyNoWriNo/MyNoWriNo/HomeViewController.swift
+++ MyNoWriNo/MyNoWriNo/HomeViewController.swift
@@ -34,6 +34,13 @@ class HomeViewController: UIViewController {
         
         
     }
+    
+    
+    @IBAction func newProjectButtonPressed(_ sender: AnyObject) {
+        self.performSegue(withIdentifier: NewProjectViewController.identifier, sender: nil)
+    }
+    
+    
 
 }
 
