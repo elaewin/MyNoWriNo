a9f12d6791270c2fcddbc9429f90fb990ffd75e9
Corey Malek <coreymalek@coreys-macbook-pro.local>
2016-11-07 10:17:24 -0800

    Added Calendar permission to info.plist, Added CalendarViewController.swift file

---
+++ MyNoWriNo/MyNoWriNo/CalendarViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CalendarViewController.swift
+//  MyNoWriNo
+//
+//  Created by Corey Malek on 11/7/16.
+//  Copyright © 2016 Erica Winberry. All rights reserved.
+//
+
+import UIKit
+import EventKit
+
+class CalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+}
