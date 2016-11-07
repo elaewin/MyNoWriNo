410ab4b2c3ff15ee61a31a87741a15be3d69bd71
Erica Winberry <elaewin@gmail.com>
2016-11-07 12:10:58 -0800

    Finished with init of Project object.

--- MyNoWriNo/MyNoWriNo/Project.swift
+++ MyNoWriNo/MyNoWriNo/Project.swift
@@ -17,23 +17,37 @@ class Project {
     var percentComplete: Int = 0
     let deadline: Date
     var currentDate: Date
+    var wordsRemaining: Int
+    var daysRemaining: Int
     
     var dailyWordCount: [Date: Int]?
     var genre: String?
     
-    var daysRemaining: TimeInterval? // make a function that subtracts the current date from deadline
-    var wordsRemaining: Int?
     var wordsPerDayForTarget: Int?
     
-    init(name: String, targetWordCount: Int, cumulativeWordCount: Int, percentComplete: Int, deadline: Date, currentDate: Date) {
+    init(name: String, targetWordCount: Int, cumulativeWordCount: Int, percentComplete: Int, deadline: Date, currentDate: Date, wordsRemaining: Int, daysRemaining: Int) {
         self.name = name
         self.targetWordCount = targetWordCount
         self.cumulativeWordCount = cumulativeWordCount
         self.percentComplete = percentComplete
-        self.deadline = deadline
-        self.currentDate = currentDate // make a function that gets the current date
+        self.deadline = deadline // how to get this date?
+        self.currentDate = NSDate() as Date
         self.wordsRemaining = self.targetWordCount - self.cumulativeWordCount
         
+        if !(self.currentDate > self.deadline) {
+            let interval = (DateInterval(start: self.currentDate, end: self.deadline)).duration / 86400
+            self.daysRemaining = Int(interval.rounded())
+        } else {
+            self.daysRemaining = 0
+        }
+        
+        self.wordsPerDayForTarget = self.wordsRemaining / self.daysRemaining
     }
-
+    
+    func getDaysRemaining(currentDate: Date, deadline: Date) {
+        let interval = DateInterval(start: self.currentDate, end: self.deadline).duration / 86400
+        self.daysRemaining = Int(interval.rounded())
+    }
+    
+    
 }
