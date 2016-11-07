b077c434473f51dd881d7b394ef45eea9a975788
Erica Winberry <elaewin@gmail.com>
2016-11-07 11:03:58 -0800

    Working on Project object.

--- MyNoWriNo/MyNoWriNo/Project.swift
+++ MyNoWriNo/MyNoWriNo/Project.swift
@@ -11,4 +11,29 @@ import UIKit
 // class for all projects.
 class Project {
     
+    let name: String
+    let targetWordCount: Int
+    var cumulativeWordCount: Int = 0
+    var percentComplete: Int = 0
+    let deadline: Date
+    var currentDate: Date
+    
+    var dailyWordCount: [Date: Int]?
+    var genre: String?
+    
+    var daysRemaining: TimeInterval? // make a function that subtracts the current date from deadline
+    var wordsRemaining: Int?
+    var wordsPerDayForTarget: Int?
+    
+    init(name: String, targetWordCount: Int, cumulativeWordCount: Int, percentComplete: Int, deadline: Date, currentDate: Date) {
+        self.name = name
+        self.targetWordCount = targetWordCount
+        self.cumulativeWordCount = cumulativeWordCount
+        self.percentComplete = percentComplete
+        self.deadline = deadline
+        self.currentDate = currentDate // make a function that gets the current date
+        self.wordsRemaining = self.targetWordCount - self.cumulativeWordCount
+        
+    }
+
 }
