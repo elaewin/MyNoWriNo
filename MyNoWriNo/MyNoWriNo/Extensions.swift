0e8870dc2a4fc506c0c5e814994f91886f6717fa
Erica Winberry <elaewin@gmail.com>
2016-11-07 16:03:37 -0800

    working on Create New Project VC - getting data from text fields.

--- MyNoWriNo/MyNoWriNo/Extensions.swift
+++ MyNoWriNo/MyNoWriNo/Extensions.swift
@@ -8,9 +8,44 @@
 
 import UIKit
 
+extension String {
+    
+    var textIsValid: Bool {
+        
+        let regexPattern = "[^0-9a-z]"
+        
+        do {
+            let regex = try NSRegularExpression(pattern: regexPattern, options: .caseInsensitive)
+            let range = NSRange(location: 0, length: (self.characters.count))
+            let matches = regex.numberOfMatches(in: self, options: .reportCompletion, range: range)
+            if matches > 0 { return false }
+        } catch {
+            return false
+        }
+        return true
+    }
+    
+    var numberIsValid: Bool {
+        
+        let regexPattern = "[^0-9]"
+        
+        do {
+            let regex = try NSRegularExpression(pattern: regexPattern, options: .caseInsensitive)
+            let range = NSRange(location: 0, length: (self.characters.count))
+            let matches = regex.numberOfMatches(in: self, options: .reportCompletion, range: range)
+            if matches > 0 { return false }
+        } catch {
+            return false
+        }
+        return true
+    }
+    
+}
+
 extension UIResponder {
     
     static var identifier: String {
         return String(describing: self)
     }
 }
+
--- MyNoWriNo/MyNoWriNo/NewProjectViewController.swift
+++ MyNoWriNo/MyNoWriNo/NewProjectViewController.swift
@@ -10,24 +10,44 @@ import UIKit
 
 class NewProjectViewController: UIViewController {
     
+    var newProject: Project?
+    
+    var newDate: Date?
     
     @IBOutlet weak var newProjectView: UIView!
     
+    @IBOutlet weak var projectTitleTextField: UITextField!
     
+    @IBOutlet weak var deadlineDatePicker: UIDatePicker!
     
+    @IBOutlet weak var wordCountTextField: UITextField!
     
-    
-    
+    @IBOutlet weak var genreTextField: UITextField!
 
+    @IBOutlet weak var createButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        deadlineDatePicker.minimumDate = Date()
+        
+        self.projectTitleTextField.delegate = self
+        self.wordCountTextField.delegate = self
+        self.genreTextField.delegate = self
     }
     
-    
+    @IBAction func newDueDateSelected(_ sender: Any) {
+        newDate = deadlineDatePicker.date
+        print("New Date Selected is: \(newDate)")
+    }
     
     @IBAction func createButtonPressed(_ sender: Any) {
+        
+        if projectTitleTextField.text != "" && wordCountTextField.text != "" {
+            print("Create button pressed")
+            
+        }
         
         dismiss(animated: true, completion: nil)
     }
@@ -35,3 +55,20 @@ class NewProjectViewController: UIViewController {
 
 
 }
+
+extension NewProjectViewController: UITextFieldDelegate {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if projectTitleTextField.text != "" && wordCountTextField.text != "" {
+            createButton.isEnabled = true
+        }
+    }
+
+}
+
+
+
+
+
+
+
