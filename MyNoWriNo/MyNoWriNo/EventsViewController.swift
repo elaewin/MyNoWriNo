7f1d049671b08dfc059d8d52704858a1798823a1
Corey Malek <coreymalek@coreys-macbook-pro.local>
2016-11-10 08:31:42 -0800

    Added AboutViewController and about page to storyboard

--- MyNoWriNo/MyNoWriNo/EventsViewController.swift
+++ MyNoWriNo/MyNoWriNo/EventsViewController.swift
@@ -9,11 +9,38 @@
 import UIKit
 
 class EventsViewController: UIViewController {
+    
+    var mainTimer: Timer!
+    var secondsRemaining: TimeInterval!
+    
+    @IBOutlet weak var sprintTimer: UIDatePicker!
 
+    
+    @IBAction func startTimer(_ sender: Any) {
+        
+        self.secondsRemaining = self.sprintTimer.countDownDuration
+        
+        self.mainTimer = Timer.scheduledTimer(withTimeInterval: self.secondsRemaining, repeats: false, block: @escaping )
+        
+    }
+    
+    
+    @IBAction func stopTimer(_ sender: Any) {
+        
+    }
+    
+    
+    func finishMainTimer() {
+        self.stopTimer(mainTimer)
+    }
+    
+    
+    
     var project: Project!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         if let projectTabBarController = self.tabBarController as? ProjectTabBarController {
             self.project = projectTabBarController.project
