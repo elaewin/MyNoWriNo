6b386e83b487a1ac0192eb5db06ad185e1f03bb2
Corey Malek <coreymalek@coreys-macbook-pro.local>
2016-11-10 14:50:31 -0800

    Fixed constraints in projectCell.xib, detailsTab

---
+++ MyNoWriNo/MyNoWriNo/AboutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutViewController.swift
+//  MyNoWriNo
+//
+//  Created by Corey Malek on 11/10/16.
+//  Copyright © 2016 Erica Winberry. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+   
+}
